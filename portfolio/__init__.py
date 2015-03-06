28ca8e23aa9097f6218a6ba2a8c672276ec79288
Eduardo Cuducos <cuducos@gmail.com>
2015-03-06 12:34:18 -0700

    Remove coffee bin config

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -19,7 +19,6 @@ app.jinja_env.lstrip_blocks = True
 # assets
 assets = Environment(app)
 assets.config['PYSCSS_LOAD_PATHS'] = app.config['PYSCSS_LOAD_PATH']
-assets.config['coffee_bin'] = app.config['COFFEE_BIN']
 assets.load_path = app.config['WEBASSETS_LOAD_PATH']
 assets.from_yaml(app.config['ASSETS'])
 
