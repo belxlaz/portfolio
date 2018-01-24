6ea54ad31ee4368216a7a3cbc6e0bf7229dec57c
Eduardo Cuducos <cuducos@gmail.com>
2018-01-24 08:41:48 -0200

    Remove Flask-Script and Flask-Freezer

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -1,12 +1,9 @@
 from flask import Flask
 from flask.ext.assets import Environment
-from flask.ext.frozen import Freezer
-from flask.ext.script import Manager
 from portfolio.views import site
 
 # init the app, its compressor and its manager
 app = Flask('portfolio')
-manager = Manager(app)
 
 # config
 app.config.from_pyfile('config.py')
@@ -22,15 +19,3 @@ assets.from_yaml(app.config['ASSETS'])
 
 # load views
 app.register_blueprint(site)
-
-# freezer
-FreezerCommand = Manager(usage='Frozen-Flask commands')
-
-
-@FreezerCommand.command
-def freeze():
-    """Freeze Flask application"""
-    freezer = Freezer(app)
-    freezer.freeze()
-
-manager.add_command('freezer', FreezerCommand)
